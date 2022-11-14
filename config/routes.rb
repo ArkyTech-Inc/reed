c83277aa0973375e6043a4b5bdccea08f125140b
ArkyTech <motolanisomoye@gmail.com>
2022-11-14 04:19:12 +0100

    Adding chat features, 20% done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
   resources :posts
   devise_for :users
   resources :pins
+  resources :rooms
  # get 'home/index'
   root "home#index"
   #root "pins#index"
   get 'home/about'
   get 'home/pin'
+
+  devise_scope :user do
+    get "users", to: "devise/sessions#new"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
